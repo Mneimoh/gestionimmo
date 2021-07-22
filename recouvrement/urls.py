7a911fb8022c2545894d2852e243ad25963d2856
honorezemagho <honorezemagho@gmail.com>
2021-07-22 12:33:23 +0100

    finish with recouvrement part

--- recouvrement/urls.py
+++ recouvrement/urls.py
@@ -3,15 +3,58 @@ from django.urls import path
 
 
 urlpatterns = [
+    
+    #Direction du recouvrement paths
     path('', views.index, name='recouvrement'),
     path('etat/', views.etat_recouvrements, name='etat_recouvrement'),
     path('contacts/', views.contacts, name='contacts'),
     path('litiges/', views.litiges, name='litiges'),
     path('finalisations/', views.finalisations, name='finalisations'),
     path('rapports/', views.rapports, name='rapports'),
+    
+    # Etats dossiers paths
     path('dossiers-recouvrement/', views.etat_dossiers_recouvrement, name='dossier-recouvrement'),
     path('dossiers-statistique/', views.etat_dossiers_statistique, name='dossier-statistique'),
     path('dossiers-profil/', views.etat_profil_dossiers, name='dossier-profil'),
     path('dossiers-etats-complets/', views.etat_dossiers_recouvrement_complets, name='dossier-recouvrement-etats-complets'),
-]
+    
+    #Contacts paths
+    path('contacts-rappel', views.contact_rappel, name='contacts-rappel'),
+    path('contacts-sms', views.contact_sms, name='contacts-sms'),
+    path('contacts-autosms', views.contact_autosms, name='contacts-autosms'),
+    path('contacts-emails', views.contact_emails, name='contacts-emails'),
+    path('contacts-autoemails', views.contact_autoemails, name='contacts-autoemails'),
+    path('contacts-annotation', views.contact_annotation, name='contacts-annotation'),
+    
+    #Gestion Finalisation paths
+    path('gestion-verification-paiement', views.gestion_verification_paiement, name='gestion-verification-paiement'),
+    path('gestion-attestation', views.gestion_attestation, name='gestion-attestation'),
+    path('gestion-pml', views.gestion_pml, name='gestion-pml'),
+    path('gestion-felicitations', views.gestion_felicitations, name='gestion-felicitations'),
+    path('gestion-attributions', views.gestion_attributions, name='gestion-attributions'),
+    
+    #props terriers , modif restructure path
+    path('prop-terriens', views.prop_terriens, name='prop-terriens'),
+    path('modif-restructure', views.modif_restructure, name='modif-restructure'),
+    
+    #annulation paths
+    path('annulation-auto', views.annulation_auto, name='annulation-auto'),
+    path('annulation-courrier', views.annulation_courrier, name='annulation-courrier'),
+    path('annulation-finalisation', views.annulation_finalisation, name='annulation-finalisation'),
+    
+    #delocalisation paths
+    path('delocalisation-auto', views.delocalisation_auto, name='delocalisation-auto'),
+    path('delocalisation-courrier', views.delocalisation_courrier, name='delocalisation-courrier'),
+    path('delocalisation-finalisation', views.delocalisation_finalisation, name='delocalisation-finalisation'),
 
+    #delocalisation paths
+    path('gestionpr-1', views.gestionpr_1, name='gestionpr-1'),
+    path('gestionpr-2', views.gestionpr_2, name='gestionpr-2'),
+    path('gestion-vi-1', views.gestion_vi_1, name='gestion-vi-1'),
+    path('gestion-vi-2', views.gestion_vi_2, name='gestion-vi-2'),
+    path('gestion-rejets', views.gestion_rejets, name='gestion-rejets'),
+    
+    #repechage paths
+    path('repechage-auto', views.repechage_auto, name='repechage-auto'),
+    path('repechage-2', views.repechage_2, name='repechage-2'),
+]
--- recouvrement/views.py
+++ recouvrement/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Direction du recouvrement routes
 
 def index(request):
     return render(request,'recouvrement/recouvrement.html', { 'title': 'Recouvrement'})
@@ -22,6 +22,8 @@ def finalisations(request):
 def rapports(request): 
     return render(request, 'recouvrement/direction/rapports.html', { 'title': ' Rapports'})
 
+# Etats dossiers routes
+
 def etat_dossiers_recouvrement (request): 
     return render(request, 'recouvrement/etats-dossiers/recouvrement.html', { 'title': ' Etat des recouvrements'})
 
@@ -34,3 +36,94 @@ def etat_profil_dossiers (request):
 def etat_dossiers_recouvrement_complets (request): 
     return render(request, 'recouvrement/etats-dossiers/etats-complets.html', { 'title': ' Etat dossiers complets'})
 
+
+# Contact routes
+def contact_rappel(request): 
+    return render(request, 'recouvrement/contacts/rappel.html', { 'title': ' Appel Manuels'})
+
+def contact_sms(request): 
+    return render(request, 'recouvrement/contacts/sms.html', { 'title': ' SMS Manuels'})
+
+def contact_autosms(request): 
+    return render(request, 'recouvrement/contacts/autosms.html', { 'title': 'Auto SMS'})
+
+def contact_emails(request): 
+    return render(request, 'recouvrement/contacts/emails.html', { 'title': ' Email Manuels'})
+
+def contact_autoemails(request): 
+    return render(request, 'recouvrement/contacts/autoemails.html', { 'title': ' Auto Email'})
+
+def contact_annotation(request): 
+    return render(request, 'recouvrement/contacts/annotation.html', { 'title': ' Annotation'})
+
+
+# Gestion finalisation routes
+def gestion_verification_paiement(request): 
+    return render(request, 'recouvrement/gestion-finalisation/verification-paiement.html', { 'title': 'Verification de fin de paiement'})
+
+def gestion_attestation(request): 
+    return render(request, 'recouvrement/gestion-finalisation/attestation.html', { 'title': ' Attestion de fin de paiement'})
+
+def gestion_pml(request): 
+    return render(request, 'recouvrement/gestion-finalisation/pml.html', { 'title': 'Emissions plans de masses locaux'})
+
+def gestion_felicitations(request): 
+    return render(request, 'recouvrement/gestion-finalisation/felicitations.html', { 'title': 'Certificats de félicitations'})
+
+def gestion_attributions(request): 
+    return render(request, 'recouvrement/gestion-finalisation/attributions.html', { 'title': "Certificats d'attributions"})
+
+
+# (proprieraires terriens, modif restructure) routes
+def prop_terriens(request): 
+    return render(request, 'recouvrement/credit-prop-terriens.html', { 'title': "Dossiers crédits prop. terriens"})
+
+def modif_restructure(request): 
+    return render(request, 'recouvrement/modif-restructure.html', { 'title': "Modification & Restructuration"})
+
+
+# annulation routes
+def annulation_auto(request): 
+    return render(request, 'recouvrement/annulation/automatique.html', { 'title': "Annulation automatique"})
+
+def annulation_courrier(request): 
+    return render(request, 'recouvrement/annulation/courrier.html', { 'title': "Annulation par courrier"})
+
+def annulation_finalisation(request): 
+    return render(request, 'recouvrement/annulation/finalisation.html', { 'title': "Annulation finalisation"})
+
+
+# delocalisation routes
+def delocalisation_auto(request): 
+    return render(request, 'recouvrement/delocalisation/automatique.html', { 'title': "Delocalisation automatique"})
+
+def delocalisation_courrier(request): 
+    return render(request, 'recouvrement/delocalisation/courrier.html', { 'title': "Delocalisation par courrier"})
+
+def delocalisation_finalisation(request): 
+    return render(request, 'recouvrement/delocalisation/finalisation.html', { 'title': "Delocalisation finalisation"})
+
+
+# sinistre routes
+def gestionpr_1(request): 
+    return render(request, 'recouvrement/sinistre/gestionpr-1.html', { 'title': "Gestion sinistre phase 1 (Reception sinistre et recherche)"})
+
+def gestionpr_2(request): 
+    return render(request, 'recouvrement/sinistre/gestionpr-2.html', { 'title': "Gestion sinistre phase 2 (acceptée)"})
+
+def gestion_vi_1(request): 
+    return render(request, 'recouvrement/sinistre/gestion-vi-1.html', { 'title': "Gestion sinistre phase 1 (Reception sinistre et recherche)"})
+
+def gestion_vi_2(request): 
+    return render(request, 'recouvrement/sinistre/gestion-vi-1.html', { 'title': "Gestion sinistre phase 2 (acceptée)"})
+
+def gestion_rejets(request): 
+    return render(request, 'recouvrement/sinistre/gestion-rejets.html', { 'title': "Gestion sinistre phase 2 (rejetée)"})
+
+
+# repechage routes
+def repechage_auto(request): 
+    return render(request, 'recouvrement/repechage/automatique.html', { 'title': "Repêchage 1"})
+
+def repechage_2(request): 
+    return render(request, 'recouvrement/repechage/repechage2.html', { 'title': "Repêchage 2"})
