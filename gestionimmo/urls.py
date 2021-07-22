98d9e4ea47f79fa73b240907c1d5c930fb277567
eldrige <peldrige8@gmail.com>
2021-07-22 16:06:49 +0100

    fix migrations issue

--- gestionimmo/urls.py
+++ gestionimmo/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accueuil/', include('accueuil.urls')),
-    path('transac/', include('transac.urls'))
+    path('transac/', include('transac.urls')),
     path('caisse/', include('caisse.urls')),
-    path('recouvrement/', include('recouvrement.urls')),
+    path('recouvrement/', include('recouvrement.urls'))
 ]
