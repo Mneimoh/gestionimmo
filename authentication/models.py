899e6531a8795cefd03d251d980cc009e66035c4
Lontchi12 <lontchilionelle@gmail.com>
2021-07-24 23:22:31 +0100

    added user authentication

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    societe = models.IntegerField(default=0)
+    login = models.CharField(max_length=15)
+    password = models.CharField(max_length=15)
+    email = models.EmailField(max_length=60)
+    poste = models.CharField(max_length=15)
+    active = models.BooleanField(default=False)
+    date_creation = models.DateField()
+
+    def __str__(self):
+        return self.login
