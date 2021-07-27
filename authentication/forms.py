ec8874a539afc87a5f2245a2a29fcf778113b2a6
eldrige <peldrige8@gmail.com>
2021-07-28 00:49:03 +0100

    styled login form

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(max_length=60)
-    password = forms.CharField(max_length=60)
+    username = forms.CharField(max_length=60, widget=forms.TextInput(
+        attrs={
+            "class":"form-control",
+            "placeholder": "Username"
+        }
+    ))
+    password = forms.CharField(max_length=60, widget=forms.TextInput(
+        attrs={
+            "class": "form-control",
+            "placeholder": "Password",
+            "type": "password"
+        }
+    ))
