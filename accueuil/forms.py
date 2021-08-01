a04a48207e9e7b4322213d543cccf81b45f2ff79
eldrige <peldrige8@gmail.com>
2021-08-01 13:11:23 +0100

    start modeling acceuil_forms

---
+++ accueuil/forms.py
@@ -0,0 +1,101 @@
+from django import forms
+from django.forms import fields
+from main.models import Client, ClientAppel
+
+class ClientRdv(forms.ModelForm):
+
+    phone_1 = forms.CharField(
+        label = 'Numéro client',
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control', 'placeholder': '674048984'}
+        )
+    )   
+
+    nom = forms.CharField(
+        label = 'Nom',
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control', 'placeholder': 'DOSSO'}
+        )
+    )   
+
+    prenom = forms.CharField(
+        label = 'Prénom',
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control', 'placeholder': 'Hubert'}
+        )
+    )   
+
+    phone_2 = forms.CharField(
+        label = 'Téléphone',
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control', 'placeholder': '986876869'}
+        )
+    )   
+
+    HOW_CONU_CHOICES = (
+       ('PRESSE','Presse'),
+       ('TELE', 'Tele'),
+       ('RADIO','Radio'),
+       ('CONNAISANCE','Connaisance'),
+       ('AUTRE','Autre'),
+    )
+    
+    how_connu = forms.ChoiceField(
+        label = 'Comment nous a t-il connu ?',
+        choices=HOW_CONU_CHOICES,
+        required = True,
+        widget = forms.Select(
+            attrs = {'class': 'form-control'}
+        )
+    )
+
+
+    # how_connu = forms.ChoiceField(
+    #     label = 'Comment nous a t-il connu ?',
+    #     choices=HOW_CONU_CHOICES,
+    #     required = True,
+    #     widget = forms.Select(
+    #         attrs = {'class': 'form-control'}
+    #     )
+    # )    
+
+    class Meta:
+        model = Client
+        fields = ('phone_1','nom', 'prenom','how_connu', 'phone_2', 'how_connu')
+        labels={
+            'phone_1': 'Numéro client', 
+            'nom':'Nom',
+            'prenom': 'Prénom',
+            'phone_2': 'Téléphone',
+            'how_connu': 'Comment nous a t-il connu ?'
+        }
+        
+# from client appel, I will tke date dappel and date
+class ClientAppelForm(forms.ModelForm):
+    date_ap = forms.DateField(
+        label = "Date d'appel",
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control'}
+        )
+    )  
+
+    date = forms.CHarField(
+        label = "date",
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control'}
+        )
+    )  
+
+    class Meta:
+        model = ClientAppel,
+        fields = ('date_ap','date')
+        labels={
+            'date_ap':"Date d'appel",
+            'date': "Heur d'appel"
+        }
--- accueuil/views.py
+++ accueuil/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .decorators import unauthenticated_user, allowed_users
+from .forms import ClientRdv
 
 
 # Create your views here.
 @unauthenticated_user
 @allowed_users(allowed_roles=["acceuil"])
 def index(request):
-    return render(request,'accueuil/appel.html', { 'title': 'Enregistrement des appels'})
+    form = ClientRdv()
+    return render(request = request, template_name= 'accueuil/appel.html', context= { 'client_form': form, 'title': 'Enregistrement des appels'})
 
 
 @unauthenticated_user
