a58dc75a0807d364eaf003cc8b083843e6b3bf29
Mneimoh <juniornkiangmatiah@gmail.com>
2021-09-19 14:19:46 +0100

    Creating pdfs

--- gestionimmo/settings.py
+++ gestionimmo/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
